fa4b61504ffa1379029cf8b42d04f14b13b01adb
honake <honami.yuya@gmail.com>
2017-01-13 04:27:17 +0900

    Add cycle

---
+++ data_structures/cycle.py
@@ -0,0 +1,23 @@
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as:
+
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+def has_cycle(head):
+    node = head
+    memo = set()
+    while True:
+        if node == None:
+            return False
+        else:
+            if node in memo:
+                return True
+            else:
+                memo.add(node)
+                node = node.next
